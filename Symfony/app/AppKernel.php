46cb2a6a154dca8228e0e1c498a8e95fa18f6e32
MDHayden <melanie.dubreuil21@gmail.com>
2015-02-10 17:06:10 +0100

    Cleaning Symfony from OCForumBundle and Cache_dev

--- Symfony/app/AppKernel.php
+++ Symfony/app/AppKernel.php
@@ -16,7 +16,6 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new OC\ForumBundle\OCForumBundle(),
             new KEEG\WebsiteBundle\KEEGWebsiteBundle()
         );
 
