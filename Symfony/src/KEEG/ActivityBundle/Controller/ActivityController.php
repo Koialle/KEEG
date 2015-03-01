364678b06d5f1e835293e0cd92117e5443bcd2d9
Ellynk <Ophelie_Rodrigues@hotmail.com>
2015-03-01 13:18:52 +0100

    Modification controler et router ActivityBundle et création des vues questionnaires et quizz

--- Symfony/src/KEEG/ActivityBundle/Controller/ActivityController.php
+++ Symfony/src/KEEG/ActivityBundle/Controller/ActivityController.php
@@ -13,4 +13,30 @@ class ActivityController extends Controller
     	$content = $this->get('templating')->render('KEEGActivityBundle:Activities:index.html.twig');
 		return new Response($content);
     }
+	
+	public function questionnairesAction(Request $request)
+    {
+		return $this->render('KEEGActivityBundle:Activities:questionnaires.html.twig');
+    }
+	
+	public function quizzInfoAction(Request $request)
+	{
+		return $this->render('KEEGActivityBundle:Activities:quizzInfo.html.twig');
+	}
+	
+	public function quizzCursusAction(Request $request)
+	{
+		return $this->render('KEEGActivityBundle:Activities:quizzCursus.html.twig');
+	}
+	
+	public function quizzCompetencesAction(Request $request)
+	{
+		return $this->render('KEEGActivityBundle:Activities:quizzCompetences.html.twig');
+	}
+	
+	public function quizzDomainesAction(Request $request)
+	{
+		return $this->render('KEEGActivityBundle:Activities:quizzDomaines.html.twig');
+	}
+	
 }
